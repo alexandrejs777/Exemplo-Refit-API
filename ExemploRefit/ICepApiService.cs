13528da3f543b33027007be9aeb26aa65b2ffb17
AlexandreJS <alexandrejatobasiqueira@gmail.com>
2022-02-04 11:55:47 -0300

    Estudo pronto

---
+++ ExemploRefit/ICepApiService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace ExemploRefit
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ ExemploRefit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace ExemploRefit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe o seu CEP:");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações di CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.WriteLine($"\nLogradouro: {address.Logradouro}\nComplemento: {address.Complemento}" +
+                    $"\nBairro: {address.Bairro} \nLocalidade: {address.Localidade} \nUF: {address.Uf}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de cep: " + e.Message);
+            }
+        }
+    }
+}
